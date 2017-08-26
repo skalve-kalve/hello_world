989121c1823a05bd75fb0e1b32296a480d41f63e
shital kalve <skalve97@gmail.com>
2017-08-26 15:21:05 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,128 @@
+<?php
+session_start();
+?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+<title>Wel come to Online Exam</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<link href="quiz.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+<?php
+include("header.php");
+include("database.php");
+extract($_POST);
+
+if(isset($submit))
+{
+	$rs=mysql_query("select * from mst_user where login='$loginid' and pass='$pass'");
+	if(mysql_num_rows($rs)<1)
+	{
+		$found="N";
+	}
+	else
+	{
+		$_SESSION[login]=$loginid;
+	}
+}
+if (isset($_SESSION[login]))
+{
+echo "<h1 class='style8' align=center > Welcome to Online Exam</h1>";
+		echo '<table width="28%"  border="0" align="center">
+  <tr>
+    <td width="7%" height="65" valign="bottom"><img src="image/HLPBUTT2.JPG" width="50" height="50" align="middle"></td>
+    <td width="93%" valign="bottom" bordercolor="#0000FF"> <a href="sublist.php" class="style4">Subject for Quiz </a></td>
+  </tr>
+  <tr>
+    <td height="58" valign="bottom"><img src="image/DEGREE.JPG" width="43" height="43" align="absmiddle"></td>
+    <td valign="bottom"> <a href="result.php" class="style4">Result </a></td>
+  </tr>
+</table>';
+   
+		exit;
+		
+
+}
+
+
+?>
+<table width="100%" border="0">
+  <tr>
+    <td width="70%" height="25">&nbsp;</td>
+    <td width="1%" rowspan="2" bgcolor="#CC3300"><span class="style6"></span></td>
+    <td width="29%" bgcolor="#CC3333"><div align="center" class="style1" >User Login </div></td>
+  </tr>
+  <tr>
+    <td height="296" valign="top"><div align="center">
+		<div class="jumbotron">  		
+	
+<svg width="400" height="100"> 
+<rect width="400" height="300" fill="pink"  stroke-width="8" stroke="red" />
+<text fill="res" font-size="50" font-family="verdana" x="40" y="56" talign="top">Online Quiz</text> not supported</svg>
+<br><br>
+	   <h1 class="style8" style="background-color:white">Welcome to Online Quiz</h1>
+      <span class="style5"><img src="images/imagesq.jpg" width="500" height="400"><span></span>
+        <param name="movie" value="english theams two brothers.dat">
+        <param name="quality" value="high">
+        <param name="movie" value="Drag to a file to choose it.">
+        <param name="quality" value="high">
+        <param name="BGCOLOR" value="#FFFFFF">
+<p align="left" class="style5">&nbsp;</p>
+      <blockquote>
+          <p align="left" class="style5" style="font-size:500" style="font-color:blue" ><span class="style7" >Wel Come to Online 
+            exam. This Site will provide the quiz for various subject of interest. 
+            You need to login for the take the online exam.</span></p>
+      </blockquote>
+    </div></td>
+    <td valign="top"><form name="form1" method="post" action="">
+      <table width="200" border="0">
+        <tr>
+          <td><span class="style2">Login ID </span></td>
+          <td><input name="loginid" type="text" id="loginid2"></td>
+        </tr>
+        <tr>
+          <td><span class="style2">Password</span></td>
+          <td><input name="pass" type="password" id="pass2"></td>
+        </tr>
+        <tr>
+          <td colspan="2"><span class="errors">
+            <?php
+		  if(isset($found))
+		  {
+		  	echo "Invalid Username or Password";
+		  }
+		  ?>
+          </span></td>
+          </tr>
+        <tr>
+          <td colspan=2 align=center class="errors">
+		  <input name="submit" type="submit" id="submit" value="Login">		  </td>
+        </tr>
+        <tr>
+          <td colspan="2" bgcolor="#CC3300"><div align="center"><span class="style4">New User ? <a href="signup.php">Signup Free</a></span></div></td>
+          </tr>
+      </table>
+      <div align="center">
+        <p class="style5"><img src="images/images2.png" width="134" height="128">          </p>
+        </div>
+		
+		<img src="images\download.jpg" width="150" height="128">
+		<p class="style5" align="center"><img src="images\images.jpg" width="150" height="128">
+		<p class="style5" align="right"><img src="images\download (1).jpg" width="150" height="128">
+    
+    </form></td>
+  </tr>
+</table>
+
+</body>
+</html>
